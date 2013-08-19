84e8fc756d84b741efc3fccbc9aa00406dfe53e5
ivanakurecic@gmail.com <ivanakurecic@gmail.com>
2013-08-19 11:58:42 +0200

    lowestenergy changed to lowestpt

--- TimeEventStudies/TimeAnalyzer/python/CfiFile_cfi.py
+++ TimeEventStudies/TimeAnalyzer/python/CfiFile_cfi.py
@@ -2,7 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 TimeAnalysis = cms.EDAnalyzer('TimeAnalyzer',
                               acceptedParticleTypes = cms.vint32( 11,-11 ),
-                              lowestenergy=cms.double( 20.0 ),
+                              lowestpt=cms.double( 20.0 ),
 			      acceptedParentTypes = cms.vint32(23),
-//			      lowestenergyparent=cms.double( 70.0 ) 
                               )
