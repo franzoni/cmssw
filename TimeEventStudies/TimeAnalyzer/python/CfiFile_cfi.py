4608d434ee6647546c38f3ae7a1959388eda71b8
ivanakurecic@gmail.com <ivanakurecic@gmail.com>
2013-08-20 02:43:23 +0200

    20130820

--- TimeEventStudies/TimeAnalyzer/python/CfiFile_cfi.py
+++ TimeEventStudies/TimeAnalyzer/python/CfiFile_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 TimeAnalysis = cms.EDAnalyzer('TimeAnalyzer',
                               acceptedParticleTypes = cms.vint32( 11,-11 ),
-                              lowestpt=cms.double( 20.0 ),
+                              lowestpt=cms.double( 30.0 ),
 			      acceptedParentTypes = cms.vint32(23),
                               )
