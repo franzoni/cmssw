d3f23329e63e2c6a0df180250e1c0e2828c78e97
Giovanni <giovanni.franzoni@cern.ch>
2013-10-05 13:29:51 +0200

    ad wf with customized g4 for 4M MinBias

--- Configuration/PyReleaseValidation/python/relval_dvmc.py
+++ Configuration/PyReleaseValidation/python/relval_dvmc.py
@@ -9,3 +9,6 @@ workflows[20011] = ['', ['MinBiasVHS','DIGI','RECO','HARVEST']]
 
 #neutrino gun
 workflows[20012] = ['', ['NeutrinoPt2to20gun','DIGI','RECO','HARVEST']]
+
+# minbias with G4 customization 
+workflows[20013] = ['', ['MinBiasVHS-QGSP-FTFP-BERT','DIGI','RECO','HARVEST']]
