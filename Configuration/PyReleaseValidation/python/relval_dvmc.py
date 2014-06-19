8a8a92249d624a8295cfa92845e8cddcfe42dbcd
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2014-06-19 12:43:36 +0200

    use SingleNuE10 fpr nu gun

--- Configuration/PyReleaseValidation/python/relval_dvmc.py
+++ Configuration/PyReleaseValidation/python/relval_dvmc.py
@@ -11,8 +11,8 @@ workflows = Matrix()
 # plain MC minbias (sent both in 612 and 621)
 workflows[20011] = ['', ['MinBiasVHS','DIGIdvmc','RECOdvmc','HARVESTdvmc']]
 #neutrino gun
-workflows[20012] = ['', ['NeutrinoPt2to20gun','DIGIdvmc','RECOdvmc','HARVESTdvmc']]
-workflows[20012.1] = ['', ['NeutrinoPt2to20gun']]
+workflows[20012] = ['', ['SingleNuE10','DIGIdvmc','RECOdvmc','HARVESTdvmc']]
+workflows[20012.1] = ['', ['SingleNuE10']]
 # minbias MC with G4 customization 
 workflows[20013] = ['', ['MinBiasVHS-QGSP-FTFP-BERT','DIGIdvmc','RECOdvmc','HARVESTdvmc']]
 
@@ -47,11 +47,11 @@ workflows[20111] = ['',['RunZBias2012Ddvmc','RECODdvmc','HARVESTDdvmc']]
 # MC samples for comparisons
 ###############################
 # typical min bias run 2012C
-workflows[20201] = ['', ['NeutrinoPt2to20gun','DIGIPU203002Splitdvmc','RECOMIN203002dvmc','HARVESTdvmc','ALCAMIN']]  # TMP: **WHEN SUBMITTING** label needed to differentiate ProcessingString
+workflows[20201] = ['', ['SingleNuE10','DIGIPU203002Splitdvmc','RECOMIN203002dvmc','HARVESTdvmc','ALCAMIN']]  # TMP: **WHEN SUBMITTING** label needed to differentiate ProcessingString
 # high pile up
-workflows[20202] = ['', ['NeutrinoPt2to20gun','DIGIPU198588dvmc','RECOMIN198588dvmc','HARVESTdvmc','ALCAMIN']]  # TMP: **WHEN SUBMITTING** label needed to differentiate ProcessingString
+workflows[20202] = ['', ['SingleNuE10','DIGIPU198588dvmc','RECOMIN198588dvmc','HARVESTdvmc','ALCAMIN']]  # TMP: **WHEN SUBMITTING** label needed to differentiate ProcessingString
 # 25 ns to be done when GT available FIX
-workflows[20203] = ['', ['NeutrinoPt2to20gun','DIGIPU209148dvmc','RECOMIN209148dvmc','HARVESTdvmc','ALCAMIN']]  # TMP: **WHEN SUBMITTING** label needed to differentiate ProcessingString
+workflows[20203] = ['', ['SingleNuE10','DIGIPU209148dvmc','RECOMIN209148dvmc','HARVESTdvmc','ALCAMIN']]  # TMP: **WHEN SUBMITTING** label needed to differentiate ProcessingString
 # re-digi single MB for low pu
 workflows[20204] = ['', ['MinBiasVHS','DIGIPU193092dvmc','RECOMIN193092dvmc','HARVESTdvmc']]
 
