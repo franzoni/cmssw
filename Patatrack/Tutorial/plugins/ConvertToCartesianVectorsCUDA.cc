a4cd1eb699051703274dca3a8dd8235ef75c1443
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2019-12-11 15:35:41 +0100

    the actual conversion producer

--- Patatrack/Tutorial/plugins/ConvertToCartesianVectorsCUDA.cc
+++ Patatrack/Tutorial/plugins/ConvertToCartesianVectorsCUDA.cc
@@ -40,22 +40,35 @@ ConvertToCartesianVectorsCUDA::ConvertToCartesianVectorsCUDA(const edm::Paramete
 void ConvertToCartesianVectorsCUDA::produce(edm::Event& event, const edm::EventSetup& setup) {
   auto const& input = event.get(input_);
   auto elements = input.size();
+
+  // https://en.cppreference.com/w/cpp/memory/unique_ptr/make_unique
+  // https://en.cppreference.com/w/cpp/memory/unique_ptr
   auto product = std::make_unique<CartesianVectors>(elements);
 
   // allocate memory on the GPU for the cylindrical and cartesian vectors
   // fill here ...
-
+  cudavectors::CylindricalVector* gpu_input;
+  cudavectors::CartesianVector* gpu_product;
+  cudaCheck(cudaMalloc(&gpu_input,   sizeof(cudavectors::CylindricalVector) * elements));
+  cudaCheck(cudaMalloc(&gpu_product, sizeof(cudavectors::CartesianVector) * elements));
+  
   // copy the input data to the GPU
   // fill here ...
+  cudaCheck(cudaMemcpy(gpu_input, input.data(), sizeof(cudavectors::CylindricalVector) * elements, cudaMemcpyHostToDevice));
 
   // convert the vectors from cylindrical to cartesian coordinates, on the GPU
   // fill here ...
+  cudavectors::convertWrapper(gpu_input, gpu_product, elements);
 
   // copy the result from the GPU
   // fill here ...
+  cudaCheck(cudaMemcpy(product->data(), gpu_product, sizeof(cudavectors::CartesianVector) * elements, cudaMemcpyDeviceToHost));
+
 
   // free the GPU memory
   // fill here ...
+  cudaCheck(cudaFree(gpu_input));
+  cudaCheck(cudaFree(gpu_product));
 
   event.put(output_, std::move(product));
 }
