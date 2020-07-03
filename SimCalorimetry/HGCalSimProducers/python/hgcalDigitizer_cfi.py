8a1b49b1d341e03f3f0ed81335689b141f6a7f0c
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2020-07-03 19:13:04 +0200

    custom functions to activate BX-1 (needs cleanup)

--- SimCalorimetry/HGCalSimProducers/python/hgcalDigitizer_cfi.py
+++ SimCalorimetry/HGCalSimProducers/python/hgcalDigitizer_cfi.py
@@ -300,33 +300,66 @@ def HGCal_disableNoise(process):
 
 
 def HGCal_setBxMinOneThreshold(process,tr=999):
-    process.theDigitizers.hgceeDigitizer.digiCfg.feCfg.doBxMinusOneSamples      = cms.bool(True)
+#    process.theDigitizers.hgceeDigitizer.digiCfg.feCfg.doBxMinusOneSamples      = cms.bool(True)
+#    # threshold for bx-1 sample, relative to in-time threshold;
+#    # i.e. setting to 1 -> bxMinusOneThreshold = inTimeThreshold) to flag for BX-1
+#    process.theDigitizers.hgceeDigitizer.digiCfg.feCfg.bxMinusOneThreshold      = cms.double(tr)
+#
+#    process.theDigitizers.hgchefrontDigitizer.digiCfg.feCfg.doBxMinusOneSamples = cms.bool(True)
+#    process.theDigitizers.hgchefrontDigitizer.digiCfg.feCfg.bxMinusOneThreshold = cms.double(tr)
+#
+#    process.theDigitizers.hgchebackDigitizer.digiCfg.feCfg.doBxMinusOneSamples  = cms.bool(True)
+#    process.theDigitizers.hgchebackDigitizer.digiCfg.feCfg.bxMinusOneThreshold  = cms.double(tr)
+    process.mix.digitizers.hgceeDigitizer.digiCfg.feCfg.doBxMinusOneSamples  = cms.bool(True)
     # threshold for bx-1 sample, relative to in-time threshold;
     # i.e. setting to 1 -> bxMinusOneThreshold = inTimeThreshold) to flag for BX-1
-    process.theDigitizers.hgceeDigitizer.digiCfg.feCfg.bxMinusOneThreshold      = cms.double(tr)
-
-    process.theDigitizers.hgchefrontDigitizer.digiCfg.feCfg.doBxMinusOneSamples = cms.bool(True)
-    process.theDigitizers.hgchefrontDigitizer.digiCfg.feCfg.bxMinusOneThreshold = cms.double(tr)
-
-    process.theDigitizers.hgchebackDigitizer.digiCfg.feCfg.doBxMinusOneSamples  = cms.bool(True)
-    process.theDigitizers.hgchebackDigitizer.digiCfg.feCfg.bxMinusOneThreshold  = cms.double(tr)
+    process.mix.digitizers.hgceeDigitizer.digiCfg.feCfg.bxMinusOneThreshold  = cms.double(tr)
+    process.mix.digitizers.hgchefrontDigitizer.digiCfg.feCfg.doBxMinusOneSamples  = cms.bool(True)
+    process.mix.digitizers.hgchefrontDigitizer.digiCfg.feCfg.bxMinusOneThreshold  = cms.double(tr)
+    process.mix.digitizers.hgchebackDigitizer.digiCfg.feCfg.doBxMinusOneSamples  = cms.bool(True)
+    process.mix.digitizers.hgchebackDigitizer.digiCfg.feCfg.bxMinusOneThreshold  = cms.double(tr)
     return process
 
-def HGCal_setBxMinOneThreshold__2(process):
+def HGCal_setBxMinOneThreshold_2(process):
     return HGCal_setBxMinOneThreshold(process,tr=2)
 
+def HGCal_setBxMinOneThreshold_3(process):
+    return HGCal_setBxMinOneThreshold(process,tr=3)
 
-def HGCal_setBxMinOneThreshold_2(process):
-    process.theDigitizers.hgceeDigitizer.digiCfg.feCfg.doBxMinusOneSamples      = cms.bool(True)
+def HGCal_setBxMinOneThreshold_4(process):
+    return HGCal_setBxMinOneThreshold(process,tr=4)
+
+#def HGCal_setBxMinOneThreshold__2clever(process):
+#    # process.hgcROCSettings.doBxMinusOneSamples    = cms.bool(False)   # GFGF no
+#    process.hgceeDigitizer.digiCfg.feCfg.doBxMinusOneSamples    = cms.bool(True)
+#    process.hgchefrontDigitizer.digiCfg.feCfg.doBxMinusOneSamples    = cms.bool(True)
+#    process.hgchebackDigitizer.digiCfg.feCfg.doBxMinusOneSamples    = cms.bool(True)
+#    return process
+
+def HGCal_setBxMinOneThreshold__2(process):
+##     process.theDigitizers.hgceeDigitizer.digiCfg.feCfg.doBxMinusOneSamples      = cms.bool(True)
+##     # threshold for bx-1 sample, relative to in-time threshold;
+##     # i.e. setting to 1 -> bxMinusOneThreshold = inTimeThreshold) to flag for BX-1
+##     process.theDigitizers.hgceeDigitizer.digiCfg.feCfg.bxMinusOneThreshold      = cms.double(2.)
+## 
+##     process.theDigitizers.hgchefrontDigitizer.digiCfg.feCfg.doBxMinusOneSamples = cms.bool(True)
+##     process.theDigitizers.hgchefrontDigitizer.digiCfg.feCfg.bxMinusOneThreshold = cms.double(2.)
+## 
+##     process.theDigitizers.hgchebackDigitizer.digiCfg.feCfg.doBxMinusOneSamples  = cms.bool(True)
+##     process.theDigitizers.hgchebackDigitizer.digiCfg.feCfg.bxMinusOneThreshold  = cms.double(2.)
+## 
+##     process.hgceeDigitizer.digiCfg.feCfg.doBxMinusOneSamples    = cms.bool(True)
+##     process.hgchefrontDigitizer.digiCfg.feCfg.doBxMinusOneSamples    = cms.bool(True)
+##     process.hgchebackDigitizer.digiCfg.feCfg.doBxMinusOneSamples    = cms.bool(True)
+## 
+    process.mix.digitizers.hgceeDigitizer.digiCfg.feCfg.doBxMinusOneSamples  = cms.bool(True)
     # threshold for bx-1 sample, relative to in-time threshold;
     # i.e. setting to 1 -> bxMinusOneThreshold = inTimeThreshold) to flag for BX-1
-    process.theDigitizers.hgceeDigitizer.digiCfg.feCfg.bxMinusOneThreshold      = cms.double(2.)
-
-    process.theDigitizers.hgchefrontDigitizer.digiCfg.feCfg.doBxMinusOneSamples = cms.bool(True)
-    process.theDigitizers.hgchefrontDigitizer.digiCfg.feCfg.bxMinusOneThreshold = cms.double(2.)
-
-    process.theDigitizers.hgchebackDigitizer.digiCfg.feCfg.doBxMinusOneSamples  = cms.bool(True)
-    process.theDigitizers.hgchebackDigitizer.digiCfg.feCfg.bxMinusOneThreshold  = cms.double(2.)
+    process.mix.digitizers.hgceeDigitizer.digiCfg.feCfg.bxMinusOneThreshold  = cms.double(2.)
+    process.mix.digitizers.hgchefrontDigitizer.digiCfg.feCfg.doBxMinusOneSamples  = cms.bool(True)
+    process.mix.digitizers.hgchefrontDigitizer.digiCfg.feCfg.bxMinusOneThreshold  = cms.double(2.)
+    process.mix.digitizers.hgchebackDigitizer.digiCfg.feCfg.doBxMinusOneSamples  = cms.bool(True)
+    process.mix.digitizers.hgchebackDigitizer.digiCfg.feCfg.bxMinusOneThreshold  = cms.double(2.)
     return process
 
 
